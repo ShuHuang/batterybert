2c10a5fae0c673f856469f787093e7eeafdcdf5b
Shu Huang <sh2009@cam.ac.uk>
2022-01-02 02:11:33 +0800

    tokenizer + dataset comment

--- batterybert/pretrain/datasets.py
+++ batterybert/pretrain/datasets.py
@@ -16,6 +16,14 @@ class PretrainDataset:
     """
 
     def __init__(self, train_root, validation_root, tokenizer_root, cache_root=None, block_size=128):
+        """
+        Required parameters for PretrainDataset
+        :param train_root: training text file for BatteryBERT pre-training
+        :param validation_root: test text file for BatteryBERT pre-training
+        :param tokenizer_root: pre-trained or default vocab file of tokenizer
+        :param cache_root: cache directory of transformers
+        :param block_size: block size for the text
+        """
         self.train_root = train_root
         self.validation_root = validation_root
         self.cache_root = cache_root
@@ -23,6 +31,11 @@ class PretrainDataset:
         self.block_size = block_size
 
     def group_texts(self, examples):
+        """
+        Help function for group texts
+        :param examples: Dataset object of example files
+        :return: grouped text as Dataset
+        """
         # Concatenate all texts.
         concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
         total_length = len(concatenated_examples[list(examples.keys())[0]])
@@ -38,19 +51,27 @@ class PretrainDataset:
         return result
 
     def get_dataset(self):
+        """
+        Load the pretrained file from .txt into Dataset
+        :return: Dataset
+        """
         pretrain_datasets = load_dataset("text",
                                          data_files={"train": self.train_root, "validation": self.validation_root},
                                          cache_dir=self.cache_root)
         return pretrain_datasets
 
-    def get_tokenized_datasets(self):
+    def get_tokenized_datasets(self, batched=True, batch_size=1000, num_proc=4):
+        """
+        Get tokenized language model datasets
+        :return: Tokenized Dataset of grouped text
+        """
         pretrain_tokenizer = PretrainTokenizer(self.tokenizer_root, self.cache_root, do_lower_case=True)
         datasets = self.get_dataset()
         tokenized_datasets = datasets.map(pretrain_tokenizer.tokenize_function, batched=True, num_proc=4,
                                           remove_columns=["text"])
         lm_datasets = tokenized_datasets.map(
             self.group_texts,
-            batched=True,
-            batch_size=1000,
-            num_proc=4)
+            batched=batched,
+            batch_size=batch_size,
+            num_proc=num_proc)
         return lm_datasets
--- batterybert/pretrain/tokenizer.py
+++ batterybert/pretrain/tokenizer.py
@@ -15,15 +15,28 @@ class PretrainTokenizer:
     """
 
     def __init__(self, tokenizer_root, cache_root=None, do_lower_case=True):
+        """
+        Required parameters for PretrainTokenizer
+        :param tokenizer_root: pre-trained or default vocab file of tokenizer
+        :param cache_root: cache directory of transformers
+        :param do_lower_case: cased or uncased words for language model pre-training
+        """
         self.tokenizer = tokenizer_root
         self.do_lower_case = do_lower_case
         self.cache_root = cache_root
 
     def tokenize_function(self, examples):
+        """
+        Tokenize the Dataset object file
+        """
         bert_tokenizer = self.get_tokenizer()
         return bert_tokenizer(examples["text"])
 
     def get_tokenizer(self):
+        """
+        Get the tokenizer
+        :return: Tokenizer
+        """
         pretrain_tokenizer = BertTokenizer.from_pretrained(self.tokenizer, do_lower_case=self.do_lower_case,
                                                            cache_dir=self.cache_root)
         return pretrain_tokenizer
