decaf398d36c8cfab7a985673705c0d4cc82796b
Shu Huang <sh2009@cam.ac.uk>
2022-01-03 22:14:04 +0800

    staticmethod

--- batterybert/pretrain/dataset.py
+++ batterybert/pretrain/dataset.py
@@ -15,34 +15,34 @@ class PretrainDataset:
     Dataset used for pre-training BatteryBERT.
     """
 
-    def __init__(self, train_root, validation_root, tokenizer_root, cache_root=None, block_size=128):
+    def __init__(self, train_root, validation_root, tokenizer_root, cache_root=None):
         """
         Required parameters for PretrainDataset
         :param train_root: training text file for BatteryBERT pre-training
         :param validation_root: test text file for BatteryBERT pre-training
         :param tokenizer_root: pre-trained or default vocab file of tokenizer
         :param cache_root: cache directory of transformers
-        :param block_size: block size for the text
         """
         self.train_root = train_root
         self.validation_root = validation_root
         self.cache_root = cache_root
         self.tokenizer_root = tokenizer_root
-        self.block_size = block_size
 
-    def group_texts(self, examples):
+    @staticmethod
+    def group_texts(examples, block_size=128):
         """
         Help function for group texts
         :param examples: Dataset object of example files
+        :param block_size: block size for the text
         :return: grouped text as Dataset
         """
         # Concatenate all texts.
         concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
         total_length = len(concatenated_examples[list(examples.keys())[0]])
-        total_length = (total_length // self.block_size) * self.block_size
+        total_length = (total_length // block_size) * block_size
         # Split by chunks of max_len.
         result = {
-            k: [t[i: i + self.block_size] for i in range(0, total_length, self.block_size)]
+            k: [t[i: i + block_size] for i in range(0, total_length, block_size)]
             for k, t in concatenated_examples.items()
         }
         result["labels"] = result["input_ids"].copy()
@@ -66,7 +66,7 @@ class PretrainDataset:
         pretrain_tokenizer = PretrainTokenizer(self.tokenizer_root, self.cache_root, do_lower_case=True)
         dataset = self.get_dataset()
         tokenized_datasets = dataset.map(pretrain_tokenizer.tokenize_function, batched=True, num_proc=4,
-                                          remove_columns=["text"])
+                                         remove_columns=["text"])
         lm_datasets = tokenized_datasets.map(
             self.group_texts,
             batched=batched,
